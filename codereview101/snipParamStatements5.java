32f6d9dc89117069e58739e5ae993139f4c3947b
rusosky <48994289+rusosky@users.noreply.github.com>
2024-01-17 23:36:23 -0500

    Create snipParamStatements5.java
    New file for labs

---
+++ codereview101/snipParamStatements5.java
@@ -0,0 +1,23 @@
+
+public class SQLConnection {
+    public static void main(String[] args) {
+        String userInput = "'; DROP TABLE users; --";
+        
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydatabase", "username", "password");
+            String query = "SELECT * FROM users WHERE username = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, userInput);
+            
+            ResultSet result = preparedStatement.executeQuery();
+            
+            while (result.next()) {
+                System.out.println("User: " + result.getString("username"));
+            }
+            
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
